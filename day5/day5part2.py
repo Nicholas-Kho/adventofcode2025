c00c598b9b4df178cadbaca96c1820c0b34a6953
Nick <nicholasakho@gmail.com>
2026-02-25 23:36:38 +1300

    day 5 part 2

--- day5/day5part2.py
+++ day5/day5part2.py
@@ -2,11 +2,9 @@ with open("day5.txt") as f:
     lines = f.read()
 
 def mergeFirstRange(ranges):
-    #print(ranges, "hi")
     l = ranges[0][0]
     r = ranges[0][1]
-    #print(ranges, l, r)
-    #print(ranges)
+    print(ranges)
     while True:
         for i in range(1, len(ranges)):
             replace = False
@@ -21,15 +19,17 @@ def mergeFirstRange(ranges):
                     ranges[i][0] = l
                 replace = True
             elif (l <= ranges[i][0] and r >= ranges[i][1]):
-                ranges[i] = (l, r)
-                replace = True
+                ranges.pop(i)
+                ranges.insert(0, (l, r))
+                return ranges
             elif (ranges[i][0] <= l and ranges[i][1] >= r):
                 ranges.pop(0)
                 return ranges
             if replace:
+                t = ranges.pop(i)
                 ranges.pop(0)
+                ranges.insert(0, t)
                 return mergeFirstRange(ranges)
-        #print("returning", ranges)
         return ranges
 
 ranges = lines.split("\n\n")[0]
@@ -37,7 +37,6 @@ unique = []
 for r in ranges.split("\n"):
     l = int(r.split("-")[0])
     r = int(r.split("-")[1])
-    #print(l, r)
     unique.insert(0, [l, r])
     unique = mergeFirstRange(unique)
 
